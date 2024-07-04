b907abfe1bb18cd33b08bb9d86063695cc843c58
Mikegasti <avelpogi10@gmail.com>
2024-07-04 15:42:42 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var slider = tns({
+    container: "fordSlider__slider",
+    items: 1,
+    speed: 200,
+    gutter: 100,
+    slideBy: "page",
+    autoplay: true,
+    navPosition: "bottom",
+    containerControls: "#controls",
+  });
+  
