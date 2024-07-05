a8f446a73284b2edccc435a710837893e3ace285
Mikegasti <avelpogi10@gmail.com>
2024-07-05 12:05:01 +0800

    first commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+// SLIDERRR
 var slider = tns({
-    container: "fordSlider__slider",
+    container: ".fordSlider__slider",
     items: 1,
     speed: 200,
     gutter: 100,
     slideBy: "page",
+    controls:false,
     autoplay: true,
     navPosition: "bottom",
     containerControls: "#controls",
+   
   });
   
